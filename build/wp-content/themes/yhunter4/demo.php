81af1520eb1e3cbab11d0651ee7a562e3c189ab0
Yuri Baranov <pmt.hunter@gmail.com>
2019-07-24 04:07:05 +0500

    Внедрили css grid

--- build/wp-content/themes/yhunter4/demo.php
+++ build/wp-content/themes/yhunter4/demo.php
@@ -606,9 +606,9 @@ Owl Nest Framework Demo Page
         <!-- end wrapper -->
 <!-- End of Body -->
 
-<link rel="stylesheet" href="wp-content/themes/yhunter4/css/main.min.css?rev=0.2737206646873378">
+<link rel="stylesheet" href="wp-content/themes/yhunter4/css/main.min.css?rev=0.13519828681788004">
 <script>window.jQuery || document.write('<script src="wp-content/themes/yhunter4/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="wp-content/themes/yhunter4/js/main.js?rev=0.2737206646873378"></script>
+<script src="wp-content/themes/yhunter4/js/main.js?rev=0.13519828681788004"></script>
 <script type="text/javascript">
         	 $(window).load(function () {
 			$('.gallery').featherlightGallery();
