3d253138d74929998c176a2545e545f52d2e32be
Yuri Baranov <pmt.hunter@gmail.com>
2019-08-02 00:41:31 +0500

    Дальше работаем над главной

--- build/wp-content/themes/yhunter4/demo.php
+++ build/wp-content/themes/yhunter4/demo.php
@@ -606,9 +606,9 @@ Owl Nest Framework Demo Page
         <!-- end wrapper -->
 <!-- End of Body -->
 
-<link rel="stylesheet" href="wp-content/themes/yhunter4/css/main.min.css?rev=0.13519828681788004">
+<link rel="stylesheet" href="wp-content/themes/yhunter4/css/main.min.css?rev=0.2615286051912791">
 <script>window.jQuery || document.write('<script src="wp-content/themes/yhunter4/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="wp-content/themes/yhunter4/js/main.js?rev=0.13519828681788004"></script>
+<script src="wp-content/themes/yhunter4/js/main.js?rev=0.2615286051912791"></script>
 <script type="text/javascript">
         	 $(window).load(function () {
 			$('.gallery').featherlightGallery();
--- build/wp-content/themes/yhunter4/footer.php
+++ build/wp-content/themes/yhunter4/footer.php
@@ -1,9 +1,13 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.13519828681788004">
+
+
+
+
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.2615286051912791">
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.13519828681788004"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.2615286051912791"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
