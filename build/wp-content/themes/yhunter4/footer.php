19f6eca3f8311a93f882630c3ed2f8a1912a5a08
Yuri Baranov <pmt.hunter@gmail.com>
2019-08-21 00:43:39 +0500

    Уходим в css-grid

--- build/wp-content/themes/yhunter4/footer.php
+++ build/wp-content/themes/yhunter4/footer.php
@@ -5,9 +5,9 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.03515208551606119">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.16166915909939839">
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.03515208551606119"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.16166915909939839"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
