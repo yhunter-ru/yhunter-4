2d00ad6265fbe6f62f8a5f916001f26b15e30ab1
Yuri Baranov <pmt.hunter@gmail.com>
2019-12-18 01:01:53 +0500

    Предварительно оформили блоки блога и контактов на главной

--- build/wp-content/themes/yhunter4/footer.php
+++ build/wp-content/themes/yhunter4/footer.php
@@ -5,9 +5,9 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.7055231589561166">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.8905771576677033">
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.7055231589561166"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.8905771576677033"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
@@ -20,6 +20,31 @@
 	wp_footer(); // Необходимо для нормальной работы плагинов
 ?>
 
+
+<script>
+	jQuery(document).ready(function($){  
+  element='.fit';
+  function textfit() {  
+    $(element).each(function(index) {
+      $(this).css('display', 'inline');
+      $(this).css('white-space', 'nowrap');
+      
+      wd=$(this).width();
+      wdb=$(this).parent().width();           
+      wdf=parseFloat($(this).css('font-size')); 
+
+      $(this).css('font-size', wdf*(wdb/wd));
+      $(this).css('line-height', wdf*(wdb/wd)+'px');
+    });
+  } 
+  window.onload = function() {
+  	textfit(); 
+  }
+  jQuery(window).resize(function() { 
+      textfit();
+  });
+});
+</script>
 
 </body>
 </html>
