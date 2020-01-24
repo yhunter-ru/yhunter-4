6bc1d9c16452d17530f9c3356f2cc1f7e1a3c743
Yuri Baranov <pmt.hunter@gmail.com>
2020-01-25 00:06:52 +0500

    Либо делаем мелкие картинки в портфолио, либо крупные?

--- build/wp-content/themes/yhunter4/footer.php
+++ build/wp-content/themes/yhunter4/footer.php
@@ -5,9 +5,9 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.8905771576677033">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.854744726905293">
 <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.8905771576677033"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.854744726905293"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
