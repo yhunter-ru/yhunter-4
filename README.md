26d5546bc9773a04445f09eba54a7e44b5f65cab
Yuri Baranov <pmt.hunter@gmail.com>
2022-07-22 03:00:13 +0500

    about grid in process

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Owl
+
+1) npm install
+
+2) change themepath name in gulpfile
+
+3) gulp init
+
+3) gulp watch
---
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -0,0 +1,25 @@
+
+
+
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.08926581928484367">
+<script>
+	window.jQuery || document.write(
+		'<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"><\/script>')
+</script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.08926581928484367"></script>
+<?php
+/**
+ * Чистый Шаблон для разработки
+ * Шаблон футера
+ * http://dontforget.pro
+ * @package WordPress
+ * @subpackage clean
+ */
+
+	wp_footer(); // Необходимо для нормальной работы плагинов
+?>
+
+
+</body>
+
+</html>
