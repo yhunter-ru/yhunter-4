053aa28b43c7363df96745d352979b8290fcad0e
Yuri Baranov <pmt.hunter@gmail.com>
2022-08-10 03:48:32 +0500

    intro design modification

--- build/wp-content/themes/yhunter-4/demo.php
+++ build/wp-content/themes/yhunter-4/demo.php
@@ -800,11 +800,11 @@
 			distinctio illo quasi.</p>
 	</div>
 
-	<link rel="stylesheet" href="wp-content/themes/yhunter-4/css/main.min.css?rev=0.26350418786559704">
+	<link rel="stylesheet" href="wp-content/themes/yhunter-4/css/main.min.css?rev=0.985228000532745">
 	<script>
 		window.jQuery || document.write('<script src="wp-content/themes/yhunter-4/js/vendor/jquery.min.js"><\/script>');
 	</script>
-	<script src="wp-content/themes/yhunter-4/js/main.js?rev=0.26350418786559704"></script>
+	<script src="wp-content/themes/yhunter-4/js/main.js?rev=0.985228000532745"></script>
 	<script type="text/javascript">
 		$(window).on('load', function () {
 			//$(".aligncenter, .alignnone, .alignleft, .alignright, figure img, iframe").keepTheRhythm();
