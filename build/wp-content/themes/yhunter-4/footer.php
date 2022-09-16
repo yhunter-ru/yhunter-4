d5c5dd859da46b8de597d7f307027c6145c7d7b5
Yuri Baranov <pmt.hunter@gmail.com>
2022-09-17 03:58:56 +0500

    заголовки контактов и портфолио снова привел в один вид

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -6,12 +6,12 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.8098909333135145">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.23419720244978937">
 <script>
 	window.jQuery || document.write(
 		'<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"><\/script>')
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.8098909333135145"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.23419720244978937"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
