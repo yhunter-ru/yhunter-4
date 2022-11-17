7e3cee74e59e4661ade832c40028dfa8e9478859
Yuri Baranov <pmt.hunter@gmail.com>
2022-11-18 04:23:49 +0500

    fullwidth block

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -7,12 +7,12 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.2908318573850517">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.9928471102292091">
 <script>
 	window.jQuery || document.write(
 		'<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"><\/script>')
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.2908318573850517"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.9928471102292091"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
