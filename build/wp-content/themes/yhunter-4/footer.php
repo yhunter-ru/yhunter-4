f5c65cff37bc3a01a1fc2359eb91828cfe956cb3
Yuri Baranov <pmt.hunter@gmail.com>
2022-12-06 03:32:03 +0500

    single headers variants

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -7,12 +7,12 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.7689316387137051">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.13094807114832085">
 <script>
 	window.jQuery || document.write(
 		'<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"><\/script>')
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.7689316387137051"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.13094807114832085"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
