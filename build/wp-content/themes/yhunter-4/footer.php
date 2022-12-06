f97326cfd91b8665e107cffbdb74879eece44fdd
Yuri Baranov <pmt.hunter@gmail.com>
2022-12-07 04:07:07 +0500

    portfolio archive

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -7,12 +7,12 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.6345774576064043">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.6459335973053386">
 <script>
 	window.jQuery || document.write(
 		'<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"><\/script>')
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.6345774576064043"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.6459335973053386"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
