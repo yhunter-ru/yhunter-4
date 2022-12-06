b2dea882babc99b499c3a1e5d1656c1d6404e476
Yuri Baranov <pmt.hunter@gmail.com>
2022-12-07 04:19:52 +0500

    404 ready

--- build/wp-content/themes/yhunter-4/404.php
+++ build/wp-content/themes/yhunter-4/404.php
@@ -1,12 +1,22 @@
-<?php
-/**
- * Чистый Шаблон для разработки
- * Это шаблон 404 ошибки, отрабатывает, когда написали фигни в адресную строку
- * http://dontforget.pro
- * @package WordPress
- * @subpackage clean
- */
-get_header(); // Подключаем хедер ?> 
-<h1>Ошибка 404</h1>
-<?php get_sidebar();  // Подключаем сайдбар ?>
+<?php get_header();?>
+
+
+
+
+<div class="bigheader">
+    <div class="bigheader__image">
+        <img src="/wp-content/themes/yhunter-4/img/404.gif" alt="">
+    </div>
+    <div class="bigheader__field">
+        <h1>Ошибка 404</h1>
+        <div class="bigheader__meta" style="justify-content: center">
+            Страница с таким адресом не найдена на сайте
+        </div>
+    </div>
+</div>
+
+
 <?php get_footer(); // Подключаем футер ?>
+
+
+
--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -7,12 +7,12 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.6459335973053386">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.4320813073208263">
 <script>
 	window.jQuery || document.write(
 		'<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"><\/script>')
 </script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.6459335973053386"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.4320813073208263"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
