99c583329b32b88c0030178233a2cebc0e73a70e
Yuri Baranov <pmt.hunter@gmail.com>
2022-12-29 02:57:09 +0500

    slider work-single mobile

--- build/wp-content/themes/yhunter-4/demo.php
+++ build/wp-content/themes/yhunter-4/demo.php
@@ -800,11 +800,11 @@
 			distinctio illo quasi.</p>
 	</div>
 
-	<link rel="stylesheet" href="wp-content/themes/yhunter-4/css/main.min.css?rev=0.7157417078020163">
+	<link rel="stylesheet" href="wp-content/themes/yhunter-4/css/main.min.css?rev=0.40371916511825146">
 	<script>
 		window.jQuery || document.write('<script src="wp-content/themes/yhunter-4/js/vendor/jquery.min.js"><\/script>');
 	</script>
-	<script src="wp-content/themes/yhunter-4/js/main.js?rev=0.7157417078020163"></script>
+	<script src="wp-content/themes/yhunter-4/js/main.js?rev=0.40371916511825146"></script>
 	<script type="text/javascript">
 		$(window).on('load', function () {
 			//$(".aligncenter, .alignnone, .alignleft, .alignright, figure img, iframe").keepTheRhythm();
