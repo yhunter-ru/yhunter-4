8777e47be079d1be79bb606028c21fc0d176cddd
Yuri Baranov <pmt.hunter@gmail.com>
2023-03-03 23:34:34 +0500

    local server theme init

--- build/wp-content/themes/yhunter-4/demo.php
+++ build/wp-content/themes/yhunter-4/demo.php
@@ -800,11 +800,11 @@
 			distinctio illo quasi.</p>
 	</div>
 
-	<link rel="stylesheet" href="wp-content/themes/yhunter-4/css/main.min.css?rev=0.7941488670215755">
+	<link rel="stylesheet" href="wp-content/themes/yhunter-4/css/main.min.css?rev=0.14200596230324858">
 	<script>
 		window.jQuery || document.write('<script src="wp-content/themes/yhunter-4/js/vendor/jquery.min.js"><\/script>');
 	</script>
-	<script src="wp-content/themes/yhunter-4/js/main.js?rev=0.7941488670215755"></script>
+	<script src="wp-content/themes/yhunter-4/js/main.js?rev=0.14200596230324858"></script>
 	<script type="text/javascript">
 		$(window).on('load', function () {
 			//$(".aligncenter, .alignnone, .alignleft, .alignright, figure img, iframe").keepTheRhythm();
--- build/wp-content/themes/yhunter-4/page.php
+++ build/wp-content/themes/yhunter-4/page.php
@@ -5,10 +5,12 @@
 
 
 
-<?php if ( have_posts() ) while ( have_posts() ) : the_post(); // Начало цикла ?>
-<h1><?php the_title(); // Заголовок ?></h1>
-<?php the_content(); // Содержимое страницы ?>
-<?php endwhile; // Конец цикла ?>
+<article class="page-content">
+    <?php if ( have_posts() ) while ( have_posts() ) : the_post(); // Начало цикла ?>
+    <h1><?php the_title(); // Заголовок ?></h1>
+    <?php the_content(); // Содержимое страницы ?>
+    <?php endwhile; // Конец цикла ?>
+</article>
 
 
 
