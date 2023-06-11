10b829f5cbb9b50a03a54d943727f1936aeba559
Yuri Baranov <pmt.hunter@gmail.com>
2023-06-12 03:53:45 +0500

    Дорабатываем вывод портфолио

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -131,10 +131,10 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.13953473807271544">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.7720975066412386">
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.13953473807271544"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.7720975066412386"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
