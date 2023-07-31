acf89011b6587220eefb35637ecb9e188509d93b
Yuri Baranov <pmt.hunter@gmail.com>
2023-08-01 04:55:03 +0500

    изменения заголовков и откат обратно

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -71,10 +71,10 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.1574319604195744">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.8824471985279401">
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.1574319604195744"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.8824471985279401"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
