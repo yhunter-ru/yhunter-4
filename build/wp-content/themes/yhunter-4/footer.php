c71405db18411cfec66bce063134d94c6ee7aab7
Yuri Baranov <pmt.hunter@gmail.com>
2023-11-18 05:58:46 +0500

    шаблон для фотостраницы в черном цвете

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -71,10 +71,10 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.943410130372142">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.765329832770389">
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.943410130372142"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.765329832770389"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
