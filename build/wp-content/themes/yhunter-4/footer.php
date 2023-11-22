c6631ff2a8a7738a4495a5210a6f539eb9e2b6e8
Yuri Baranov <pmt.hunter@gmail.com>
2023-11-23 00:45:12 +0500

    bugfix

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -71,10 +71,10 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.765329832770389">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.028444463218980198">
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.765329832770389"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.028444463218980198"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
