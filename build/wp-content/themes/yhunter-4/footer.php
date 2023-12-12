2539df718dea039f56405cef96c3cf1e3880470d
Yuri Baranov <pmt.hunter@gmail.com>
2023-12-12 23:26:02 +0500

    core web vitals improve

--- build/wp-content/themes/yhunter-4/footer.php
+++ build/wp-content/themes/yhunter-4/footer.php
@@ -71,10 +71,10 @@
 
 
 
-<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.11999433805377979">
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/main.min.css?rev=0.8982926435183018">
 
 <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.11999433805377979"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/main.js?rev=0.8982926435183018"></script>
 <?php
 /**
  * Чистый Шаблон для разработки
